b31df792745c85cfe727c9e53c8f6468622aaca6
varrasrivallikareddy <srivallikavarra@gmail.com>
2026-03-01 15:33:11 +0530

    Change body text color to #f5f5f5

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body style={{ margin: 0, background: 'black', color: 'white', fontFamily: 'system-ui' }}>
+      <body style={{ 
+        margin: 0, 
+        background: 'black', 
+        color: '#f5f5f5', 
+        fontFamily: 'system-ui, sans-serif'
+      }}>
         {children}
       </body>
     </html>
   );
 }
+
